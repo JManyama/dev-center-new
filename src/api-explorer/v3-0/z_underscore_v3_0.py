10642327770cd2bb40df770c868d5b4070741de9
Manyama, Jaron <jaron.manyama@sap.com>
2021-01-04 09:28:56 -0800

    update

--- src/api-explorer/v3-0/z_underscore_v3_0.py
+++ src/api-explorer/v3-0/z_underscore_v3_0.py
@@ -10,9 +10,9 @@ import os
 #os.path.abspath(r"C:\Users\I860605\Desktop\Dev_Center_New\dev_concur\src\api-explorer\v3-0")
 #os.chdir(r"C:\Users\I860605\Desktop\Dev_Center_New\dev_concur\src\api-explorer\v3-0")
 
-v3_0 = Path(r"C:\Users\I860605\Desktop\Dev_Center_New\dev_concur\src\api-explorer\v3-0").listdir()
+v3_0 = Path(r"C:\Users\I860605\Desktop\Dev_Center_New\dev_concur\src\api-explorer\v3-0")
 #Add "_" to the beginning of every markdown file that does not have it
-for markdown in v3_0:
+for markdown in v3_0.listdir():
     #os.path.join(r"C:\Users\I860605\Desktop\Dev_Center_New\dev_concur\src\api-explorer\v3-0", markdown)
     #print(markdown)
     if "_" not in markdown:
